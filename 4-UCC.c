caea2f6182e12bcd5990a2d9a0f926c9c8fec3c2
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-09-17 18:34:22 +0530

    Update 4-UCC.c

--- 4-UCC.c
+++ 4-UCC.c
@@ -12,7 +12,7 @@ int main(){
 	bzero(&servaddr,sizeof(servaddr));
 	
 	servaddr.sin_family=AF_INET;
-	servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	servaddr.sin_addr.s_addr=inet_addr("127.0.0.1 ");
 	servaddr.sin_port=htons(22000);
 
 	while(1){
